7c1b16413de99df6d2c2185e17a311c66cfe04a6
Vleena <vmascarenhas@informatica.com>
2025-05-15 09:37:06 +0530

    Directory and file creation script

---
+++ dir_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#create a directory
+mkdir dir
+
+#provide all read write execute permissions recursively
+#chmod -R 777 vleena
+
+#move to the directory
+cd dir
+
+#create two  files inside vleena directory
+touch file1 file2
+
+
+#provide all read write execute permissions recursively
+cd ..
+chmod -R 777 dir
+
+
